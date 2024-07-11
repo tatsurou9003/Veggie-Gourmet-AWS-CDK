000c6a3e828fe89d636647c4f41e514c19169f4b
tatsurou9003 <t.murakami@4nonome.com>
2024-07-11 19:12:08 +0900

    recipe_lambda prototyping

--- lambda/recipe.py
+++ lambda/recipe.py
@@ -1,9 +1,30 @@
 import json
+import boto3
 
 def recipe_lambda(event, context):
+    bedrock_runtime = boto3.client(service_name='bedrock-runtime', region_name='us-east-1')
+    model_id = 'anthropic.claude-3-sonnet-20240229-v1:0'
+    max_tokens = 1000
+    system_prompt = "必ず日本語で答えてください"
+
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": max_tokens,
+            "system": system_prompt,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "野菜を使ったレシピを教えてください"
+                }
+        ]
+        }  
+    )  
+
+    response = bedrock_runtime.invoke_model(body=body, modelId=model_id)
+    response_body = json.loads(response.get('body').read())
+
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello from the recipe lambda",
-        }),
+        'statusCode': 200,
+        'body': response_body
     }
