f52f43a72a62e12ecec3911198119d6dcf427c6c
tatsurou9003 <t.murakami@4nonome.com>
2024-07-11 19:21:06 +0900

    iam for lambda to access bedrock

--- cdk_veggie_gourmet/cdk_veggie_gourmet_stack.py
+++ cdk_veggie_gourmet/cdk_veggie_gourmet_stack.py
@@ -13,12 +13,33 @@ class CdkVeggieGourmetStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        bedrock_policy = iam.PolicyStatement(
+            effect= iam.Effect.ALLOW,
+            actions= [
+                "bedrock:*",   
+            ],
+            resources= ["*"]
+        )
+
+        lambda_role = iam.Role(
+            self,
+            "RecipeLambdaRole",
+            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+            description="Role to access Bedrock service by lambda",
+            managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name(
+                                    'service-role/AWSLambdaBasicExecutionRole'),
+                                ]
+        )
+        lambda_role.add_to_principal_policy(bedrock_policy)
+
+
         recipe_lambda = _lambda.Function(
             self, "RecipeLambda",
             runtime=_lambda.Runtime.PYTHON_3_12,
             handler="recipe.recipe_lambda",
             code=_lambda.Code.from_asset("lambda"),
             timeout=Duration.seconds(30),
+            role = lambda_role
         )
 
         api =  apigateway.LambdaRestApi(
