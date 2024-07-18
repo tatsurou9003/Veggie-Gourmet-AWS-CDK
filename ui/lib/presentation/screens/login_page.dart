7464c607b0045eb916f51ee00b9a742b06b40581
tatsurou9003 <t.murakami@4nonome.com>
2024-07-18 21:47:45 +0900

    login ui

--- ui/lib/presentation/screens/login_page.dart
+++ ui/lib/presentation/screens/login_page.dart
@@ -20,29 +20,65 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 219, 245, 153),
         appBar: AppBar(
-          title: const Text('ログイン'),
+          title: Text(_isLogin ? 'Login' : 'Sign Up'),
         ),
         body: Form(
           key: _formKey,
           child: Column(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Email'),
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'メールアドレスを入力してください';
+                  }
+                  if (!value.contains('@') || !value.contains('.')) {
+                    return '正しいメールアドレスを入力してね';
+                  }
+                  return null;
+                },
               ),
+              const SizedBox(height: 20),
               TextFormField(
-                decoration: InputDecoration(labelText: 'PassWord'),
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'パスワードを入力してください';
+                  }
+                  if (value.length < 8) {
+                    return 'パスワードは8文字以上である必要があります';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(
                 height: 40,
               ),
-              ElevatedButton(onPressed: () {}, child: Text('ログイン')),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: Text(_isLogin ? 'ログイン' : 'サインアップ'),
+              ),
               TextButton(
-                  onPressed: () {}, child: Text('Need an account? Sign up'))
+                onPressed: () {
+                  setState(() {
+                    _isLogin = !_isLogin;
+                  });
+                },
+                child: Text(_isLogin
+                    ? 'アカウント持ってないの? サインアップしてね'
+                    : 'アカウント持ってるの? ログインしてね'),
+              )
             ],
           ),
         ));
